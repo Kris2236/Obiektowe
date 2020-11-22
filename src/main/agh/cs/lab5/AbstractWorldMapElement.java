b14651ed439a525f7706ed37f1f6e669b14db31d
Kris2236 <larkfreeme70.55@gmail.com>
2020-11-22 18:56:11 +0100

    code refactore

---
+++ src/main/agh/cs/lab5/AbstractWorldMapElement.java
@@ -0,0 +1,8 @@
+package agh.cs.lab5;
+
+import java.util.ArrayList;
+
+public class AbstractWorldMapElement implements IMapElement{
+    ArrayList<Vector2d> objectsPositions = new ArrayList<>();       // map objects positions (Grass, Animals), next lab
+
+}
