464990e2c3c2ac86be13b1f0b124f78bfb2153a1
Kris2236 <larkfreeme70.55@gmail.com>
2020-11-30 00:49:33 +0100

    Added MapBoundary (Changed var name)

--- src/main/agh/cs/lab7/SimulationEngine.java
+++ src/main/agh/cs/lab7/SimulationEngine.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SimulationEngine implements IEngine{
-    private final ArrayList<MoveDirection> comands = new ArrayList<>();
+    private final ArrayList<MoveDirection> commands = new ArrayList<>();
     private final ArrayList<Vector2d> positions = new ArrayList<>();
     private final ArrayList<Animal> animals = new ArrayList<>();
     protected IWorldMap mapCurrentWorld;
 
-    public SimulationEngine(MoveDirection[] comands, IWorldMap map, Vector2d[] positions){
+    public SimulationEngine(MoveDirection[] commands, IWorldMap map, Vector2d[] positions){
 
         // Add animals move commands
-        this.comands.addAll(Arrays.asList(comands));
+        this.commands.addAll(Arrays.asList(commands));
 
         // Add animals positions to list
         this.positions.addAll(Arrays.asList(positions));
@@ -37,10 +37,10 @@ public class SimulationEngine implements IEngine{
 
     @Override
     public void run() {
-        for(int i=0; i<comands.size(); i++){
+        for(int i=0; i<commands.size(); i++){
 
             // make moves for the animals in turn
-            animals.get(i%animals.size()).move(comands.get(i));
+            animals.get(i%animals.size()).move(commands.get(i));
             System.out.println(mapCurrentWorld.toString(mapCurrentWorld));
         }
     }
